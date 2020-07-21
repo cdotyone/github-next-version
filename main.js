7c9010b7a546bf52e551f66ed3ccda11e41f90c1
Chris Doty <dotyc@civicinc.com>
2020-07-21 12:06:25 -0500

    fix: now uses all messages since last tag

--- main.js
+++ main.js
@@ -9,19 +9,6 @@ exec('git rev-list --tags --max-count=1', (err, rev, stderr) => {
         return;
     }
 
-    let message = '';
-    exec('git log -1 --pretty=%B', (err, msg, stderr) => {
-        if (err) {
-            console.log('\x1b[33m%s\x1b[0m', 'Could not find last commit message because: ');
-            console.log('\x1b[31m%s\x1b[0m', stderr);
-            process.exit(1);
-            return;
-        }
-
-        message = msg;
-        console.log('\x1b[32m%s\x1b[0m', `Found message: ${message}`);
-    });
-
     exec(`git describe --tags ${rev}`, (err, tag, stderr) => {
         if (err) {
             console.log('\x1b[33m%s\x1b[0m', 'Could not find any tags because: ');
@@ -31,32 +18,43 @@ exec('git rev-list --tags --max-count=1', (err, rev, stderr) => {
             return;
         }
 
+        tag = tag.trim();
         console.log('\x1b[32m%s\x1b[0m', `Found tag: ${tag}`);
         console.log(`::set-output name=tag::${tag}`);
 
-        var parts = tag.replace(/v/,'').trim().split('.');
-        parts.length = 3;
+        console.log(`git log ${tag}..HEAD --oneline`);
+        exec(`git log ${tag}..HEAD --oneline`, (err, message, stderr) => {
+            if (err) {
+                console.log('\x1b[33m%s\x1b[0m', 'Could not find last commit message because: ');
+                console.log('\x1b[31m%s\x1b[0m', stderr);
+                process.exit(1);
+                return;
+            }
 
-        if (message.substr(0, 4) === 'fix:') {
-            parts[2] = parseInt(parts[2]);
-            parts[2]++;
-        }
-        if (message.substr(0, 5) === 'feat:') {
-            parts[1] = parseInt(parts[1]);
-            parts[1]++;
-            parts[2] = 0;
-        }
-        if (message.indexOf("BREAKING") > 0) {
-            parts[0] = parseInt(parts[0]);
-            parts[0]++;
-            parts[1] = '0';
-            parts[2] = '0';
-        }
+            console.log('\x1b[32m%s\x1b[0m', `Found message: ${message}`);
 
-        tag = parts.join('.');
-        console.log('\x1b[32m%s\x1b[0m', `Next version: ${tag}`);
-        console.log(`::set-output name=version::${tag}`);
+            let parts = tag.replace(/v/,'').trim().split('.');
+            parts.length = 3;
 
-        process.exit(0);
+            if (message.indexOf("fix:") > 0) {
+                parts[2] = parseInt(parts[2]);
+                parts[2]++;
+            }
+            if (message.indexOf("feat:") > 0) {
+                parts[1] = parseInt(parts[1]);
+                parts[1]++;
+                parts[2] = 0;
+            }
+            if (message.indexOf("BREAKING") > 0) {
+                parts[0] = parseInt(parts[0]);
+                parts[0]++;
+                parts[1] = '0';
+                parts[2] = '0';
+            }
+
+            tag = parts.join('.');
+            console.log('\x1b[32m%s\x1b[0m', `Next version: ${tag}`);
+            console.log(`::set-output name=version::${tag}`);
+        });
     });
 });
