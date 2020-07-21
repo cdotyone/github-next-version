9132da7b33c61a316d7a6b48896f58fe8bc90125
Chris Doty <dotyc@civicinc.com>
2020-07-21 11:20:07 -0500

    feat: first commit

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # github-next-version
-Gets next version of project based on previous
+Gets next version of project based on previous commit tag and commit messages.
+
+# Get previous tag
+
+Github Action that gets the latest tag from git
+
+![Example output showing this action in action](images/output.png)
+
+## Output
+
+This action has two output: 
+    `tag` for the latest tag this action finds.
+    `version` the next version that should be used 
+
+The next version is determined by commit messages since the last tag created.
+
+Commit message conventions:
+fix: will update the 3 (patch) number of the version
+feat: will update the 2 (minor) number of the version
+BREAKING will update the 1(major) number of the version
+
+## Example
+
+```yaml
+name: Generate
+jobs:
+  generate:
+    steps:
+      - uses: actions/checkout@v1
+      - name: 'Get Previous tag'
+        id: semver
+        uses: "cdotyone/github-next-version@master"
+        env:
+          GITHUB_TOKEN: "${{ secrets.GITHUB_TOKEN }}"
+```
+
---
+++ main.js
@@ -0,0 +1,62 @@
+const { exec } = require('child_process');
+exec('git rev-list --tags --max-count=1', (err, rev, stderr) => {
+    if (err) {
+        console.log('\x1b[33m%s\x1b[0m', 'Could not find any revisions because: ');
+        console.log('\x1b[31m%s\x1b[0m', stderr);
+
+        console.log('::set-output version=tag::v1.0.0');
+        process.exit(0);
+        return;
+    }
+
+    let message = '';
+    exec('git log -1 --pretty=%B', (err, msg, stderr) => {
+        if (err) {
+            console.log('\x1b[33m%s\x1b[0m', 'Could not find last commit message because: ');
+            console.log('\x1b[31m%s\x1b[0m', stderr);
+            process.exit(1);
+            return;
+        }
+
+        message = msg;
+        console.log('\x1b[32m%s\x1b[0m', `Found message: ${message}`);
+    });
+
+    exec(`git describe --tags ${rev}`, (err, tag, stderr) => {
+        if (err) {
+            console.log('\x1b[33m%s\x1b[0m', 'Could not find any tags because: ');
+            console.log('\x1b[31m%s\x1b[0m', stderr);
+            process.exit(1);
+
+            return;
+        }
+
+        console.log('\x1b[32m%s\x1b[0m', `Found tag: ${tag}`);
+        console.log(`::set-output name=tag::${tag}`);
+
+        var parts = tag.replace(/v/,'').trim().split('.');
+        parts.length = 3;
+
+        if (message.substr(0, 4) === 'fix:') {
+            parts[2] = parseInt(parts[2]);
+            parts[2]++;
+        }
+        if (message.substr(0, 5) === 'feat:') {
+            parts[1] = parseInt(parts[1]);
+            parts[1]++;
+            parts[2] = 0;
+        }
+        if (message.indexOf("BREAKING") > 0) {
+            parts[0] = parseInt(parts[0]);
+            parts[0]++;
+            parts[1] = '0';
+            parts[2] = '0';
+        }
+
+        tag = parts.join('.');
+        console.log('\x1b[32m%s\x1b[0m', `Next version: ${tag}`);
+        console.log(`::set-output version=tag::${tag}`);
+
+        process.exit(0);
+    });
+});
