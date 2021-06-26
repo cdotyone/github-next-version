a8029e6dd484ba1f973b45c9a18dec594d94ceb8
Chris Doty <dotyc@civicinc.com>
2021-06-25 23:27:11 -0500

    chore: making more resilent

--- dist/index.js
+++ dist/index.js
@@ -1,52 +1,80 @@
-module.exports =
-/******/ (function(modules, runtime) { // webpackBootstrap
-/******/ 	"use strict";
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 129:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("child_process");;
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
 /******/ 	// The module cache
-/******/ 	var installedModules = {};
-/******/
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
 /******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
+/******/ 	function __nccwpck_require__(moduleId) {
 /******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
 /******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
 /******/ 			exports: {}
 /******/ 		};
-/******/
+/******/ 	
 /******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
+/******/ 		var threw = true;
+/******/ 		try {
+/******/ 			__webpack_modules__[moduleId](module, module.exports, __nccwpck_require__);
+/******/ 			threw = false;
+/******/ 		} finally {
+/******/ 			if(threw) delete __webpack_module_cache__[moduleId];
+/******/ 		}
+/******/ 	
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
-/******/
-/******/
-/******/ 	__webpack_require__.ab = __dirname + "/";
-/******/
-/******/ 	// the startup function
-/******/ 	function startup() {
-/******/ 		// Load entry module and return exports
-/******/ 		return __webpack_require__(104);
-/******/ 	};
-/******/
-/******/ 	// run startup
-/******/ 	return startup();
-/******/ })
+/******/ 	
 /************************************************************************/
-/******/ ({
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+const { exec } = __nccwpck_require__(129);
 
-/***/ 104:
-/***/ (function(__unusedmodule, __unusedexports, __webpack_require__) {
+const processMessage = function(message,tag) {
+    let parts = tag.replace(/v/,'').trim().split('.');
+    parts.length = 3;
 
-const { exec } = __webpack_require__(129);
+    if (message.indexOf("fix:") > 0) {
+        parts[2] = parseInt(parts[2]);
+        parts[2]++;
+    }
+    if (message.indexOf("feat:") > 0) {
+        parts[1] = parseInt(parts[1]);
+        parts[1]++;
+        parts[2] = 0;
+    }
+    if (message.indexOf("BREAKING") > 0) {
+        parts[0] = parseInt(parts[0]);
+        parts[0]++;
+        parts[1] = '0';
+        parts[2] = '0';
+    }
+
+    tag = parts.join('.');
+    console.log('\x1b[32m%s\x1b[0m', `Next version: ${tag}`);
+    console.log(`::set-output name=version::${tag}`);
+};
+
 exec('git rev-list --tags --max-count=1', (err, rev, stderr) => {
     if (err) {
         console.log('\x1b[33m%s\x1b[0m', 'Could not find any revisions because: ');
@@ -55,7 +83,6 @@ exec('git rev-list --tags --max-count=1', (err, rev, stderr) => {
         process.exit(0);
         return;
     }
-    console.log(rev);
     console.log('git describe --tags '+rev);
     exec(`git describe --tags ${rev}`, (err, tag, stderr) => {
         if (err) {
@@ -80,41 +107,12 @@ exec('git rev-list --tags --max-count=1', (err, rev, stderr) => {
             }
 
             console.log('\x1b[32m%s\x1b[0m', `Found message: ${message}`);
-
-            let parts = tag.replace(/v/,'').trim().split('.');
-            parts.length = 3;
-
-            if (message.indexOf("fix:") > 0) {
-                parts[2] = parseInt(parts[2]);
-                parts[2]++;
-            }
-            if (message.indexOf("feat:") > 0) {
-                parts[1] = parseInt(parts[1]);
-                parts[1]++;
-                parts[2] = 0;
-            }
-            if (message.indexOf("BREAKING") > 0) {
-                parts[0] = parseInt(parts[0]);
-                parts[0]++;
-                parts[1] = '0';
-                parts[2] = '0';
-            }
-
-            tag = parts.join('.');
-            console.log('\x1b[32m%s\x1b[0m', `Next version: ${tag}`);
-            console.log(`::set-output name=version::${tag}`);
+            processMessage(message, tag);
         });
     });
 });
+})();
 
-
-/***/ }),
-
-/***/ 129:
-/***/ (function(module) {
-
-module.exports = require("child_process");
-
-/***/ })
-
-/******/ });
+module.exports = __webpack_exports__;
+/******/ })()
+;
--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 const { exec } = require('child_process');
+
+const processMessage = function(message,tag) {
+    let parts = tag.replace(/v/,'').trim().split('.');
+    parts.length = 3;
+
+    if (message.indexOf("fix:") > 0) {
+        parts[2] = parseInt(parts[2]);
+        parts[2]++;
+    }
+    if (message.indexOf("feat:") > 0) {
+        parts[1] = parseInt(parts[1]);
+        parts[1]++;
+        parts[2] = 0;
+    }
+    if (message.indexOf("BREAKING") > 0) {
+        parts[0] = parseInt(parts[0]);
+        parts[0]++;
+        parts[1] = '0';
+        parts[2] = '0';
+    }
+
+    tag = parts.join('.');
+    console.log('\x1b[32m%s\x1b[0m', `Next version: ${tag}`);
+    console.log(`::set-output name=version::${tag}`);
+};
+
 exec('git rev-list --tags --max-count=1', (err, rev, stderr) => {
     if (err) {
         console.log('\x1b[33m%s\x1b[0m', 'Could not find any revisions because: ');
@@ -7,7 +33,6 @@ exec('git rev-list --tags --max-count=1', (err, rev, stderr) => {
         process.exit(0);
         return;
     }
-    console.log(rev);
     console.log('git describe --tags '+rev);
     exec(`git describe --tags ${rev}`, (err, tag, stderr) => {
         if (err) {
@@ -32,29 +57,7 @@ exec('git rev-list --tags --max-count=1', (err, rev, stderr) => {
             }
 
             console.log('\x1b[32m%s\x1b[0m', `Found message: ${message}`);
-
-            let parts = tag.replace(/v/,'').trim().split('.');
-            parts.length = 3;
-
-            if (message.indexOf("fix:") > 0) {
-                parts[2] = parseInt(parts[2]);
-                parts[2]++;
-            }
-            if (message.indexOf("feat:") > 0) {
-                parts[1] = parseInt(parts[1]);
-                parts[1]++;
-                parts[2] = 0;
-            }
-            if (message.indexOf("BREAKING") > 0) {
-                parts[0] = parseInt(parts[0]);
-                parts[0]++;
-                parts[1] = '0';
-                parts[2] = '0';
-            }
-
-            tag = parts.join('.');
-            console.log('\x1b[32m%s\x1b[0m', `Next version: ${tag}`);
-            console.log(`::set-output name=version::${tag}`);
+            processMessage(message, tag);
         });
     });
 });
